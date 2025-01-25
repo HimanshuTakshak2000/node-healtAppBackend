1a22d0f030f1ac648cfbe7948c790eebf331c30a
HimanshuTakshak2000 <himanhutakshak2000@gmail.com>
2025-01-25 22:20:38 +0530

    done some refactoring in the code

--- utils/helper.js
+++ utils/helper.js
@@ -1,16 +1,16 @@
 const sendError = (res, message) => {
-    return res.status(200).send({
-      success: false,
-      message: message,
-    });
-  };
-  
-const catchError = (res, message, error) => {
-    return res.status(500).send({
-      success: false,
-      message: message,
-      error,
-    });
-  };
+  return res.status(200).send({
+    success: false,
+    message: message,
+  });
+};
 
-  module.exports = { sendError, catchError };
+const catchError = (res, message, error) => {
+  return res.status(500).send({
+    success: false,
+    message: message,
+    error,
+  });
+};
+
+module.exports = { sendError, catchError };
